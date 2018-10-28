54089694989835e70a9017a98bd7f909b5f41824
seechinsu <seechinsu@gmail.com>
2018-10-28 11:45:38 -0400

    initial commit

---
+++ lib/project_queue.ex
@@ -0,0 +1,18 @@
+defmodule ProjectQueue do
+  @moduledoc """
+  Documentation for ProjectQueue.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProjectQueue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/project_queue_test.exs
@@ -0,0 +1,8 @@
+defmodule ProjectQueueTest do
+  use ExUnit.Case
+  doctest ProjectQueue
+
+  test "greets the world" do
+    assert ProjectQueue.hello() == :world
+  end
+end
